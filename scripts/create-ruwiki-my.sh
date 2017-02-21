ef6d719cd888decb994c244234a84d44a7d70828
Дмитрий Мурзин <dm1998@list.ru>
2017-02-21 19:23:04 +0300

    Обновил версию дампа

--- scripts/create-ruwiki-my.sh
+++ scripts/create-ruwiki-my.sh
@@ -5,4 +5,4 @@ cd ..
 # ruwiki -> ruwiki-*-pages-articles.xml.bz2
 mkdir results
 cmake --build cmake-build-debug --target converter
-curl https://dumps.wikimedia.org/ruwiki/20170201/ruwiki-20170201-pages-articles.xml.bz2 | bunzip2 | cmake-build-debug/converter >results/ruwiki-my.txt
+curl https://dumps.wikimedia.org/ruwiki/20170220/ruwiki-20170220-pages-articles.xml.bz2 | bunzip2 | cmake-build-debug/converter >results/ruwiki-my.txt
