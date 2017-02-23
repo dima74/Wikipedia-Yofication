36c0a2f3a1831badcb81c9c65d331745e821df3a
Дмитрий Мурзин <dm1998@list.ru>
2017-02-23 22:01:33 +0300

    Теперь all.sh всегда будет выполняться с -O3

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 cd `dirname $0`
+sed -i '/-g/s/^/#/w /dev/stdout' ../CMakeLists.txt | read
+sed -i '/-O3/s/^#//w /dev/stdout' ../CMakeLists.txt | read
 ./cmake-init.sh
 ./create-ruwiki-my.sh
 ./create-frequencies.sh
