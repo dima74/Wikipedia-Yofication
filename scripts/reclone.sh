409ee2f381781253b91989105e3ab4229de5c50f
Дмитрий Мурзин <dm1998@list.ru>
2017-02-24 21:44:14 +0300

    Добавил скрипт для удобного коммита замен

---
+++ scripts/reclone.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+set -x
+
+cd
+rm -rf Wikipedia-Efication
+trash-empty
+git clone git@github.com:dima74/Wikipedia-Efication.git
+cd Wikipedia-Efication
+git submodule update --init --recursive
+cd replaces
+git branch -f master b6dd96664e
+git checkout master
+mv ~/replaces .
+mv replaces/* .
+rm replaces
+git add .
+git commit -m "Замены для всех страниц Википедии"
