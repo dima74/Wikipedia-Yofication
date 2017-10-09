8509e4f2d573872d14acea22628846b91b441ea3
Дмитрий Мурзин <murzin.da@phystech.edu>
2017-10-09 16:46:01 +0300

    Минификация генерируемого файла

--- web-frontend/webpack.config.js
+++ web-frontend/webpack.config.js
@@ -1,4 +1,13 @@
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
+const copyright = `/*
+ * Скрипт-Ёфикатор для Википедии
+ * Инструкция по использованию находится по адресу https://ru.wikipedia.org/wiki/Участник:Дима74/Скрипт-Ёфикатор
+ * Историю изменений можно найти на гитхабе: https://github.com/dima74/Wikipedia-Efication/commits/master
+ * Файл сгенерирован с помощью webpack
+ */`;
 
 module.exports = {
     entry: './src/main.js',
@@ -6,8 +15,12 @@ module.exports = {
         filename: 'yoficator.bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    devtool: 'inline-source-map',
+    // devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist'
-    }
+    },
+    plugins: [
+        new UglifyJSPlugin(),
+        new webpack.BannerPlugin({banner: copyright, raw: true})
+    ]
 };
