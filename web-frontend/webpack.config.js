afb67091eed4df9b4f4e07079ba4557852794559
Дмитрий Мурзин <murzin.da@phystech.edu>
2017-10-10 15:04:25 +0300

    Отдельная webpack конфигурация для минифицированной версии

--- web-frontend/webpack.config.js
+++ web-frontend/webpack.config.js
@@ -1,13 +1,4 @@
 const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const webpack = require('webpack');
-
-const copyright = `/*
- * Скрипт-Ёфикатор для Википедии
- * Инструкция по использованию находится по адресу https://ru.wikipedia.org/wiki/Участник:Дима74/Скрипт-Ёфикатор
- * Историю изменений можно найти на гитхабе: https://github.com/dima74/Wikipedia-Efication/commits/master
- * Файл сгенерирован с помощью webpack
- */`;
 
 module.exports = {
     entry: './src/main.js',
@@ -15,12 +6,5 @@ module.exports = {
         filename: 'yoficator.bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    // devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new UglifyJSPlugin(),
-        new webpack.BannerPlugin({banner: copyright, raw: true})
-    ]
+    devtool: 'inline-source-map'
 };
---
+++ web-frontend/webpack.min.config.js
@@ -0,0 +1,18 @@
+const config = require('./webpack.config');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const copyright = `/*
+ * Скрипт-Ёфикатор для Википедии
+ * Инструкция по использованию находится по адресу https://ru.wikipedia.org/wiki/Участник:Дима74/Скрипт-Ёфикатор
+ * Историю изменений можно найти на гитхабе: https://github.com/dima74/Wikipedia-Efication/commits/master
+ * Файл сгенерирован с помощью webpack
+ */`;
+
+config.output.filename = 'yoficator.bundle.min.js';
+config.plugins = [
+    new UglifyJSPlugin(),
+    new webpack.BannerPlugin({banner: copyright, raw: true})
+];
+
+module.exports = config;
