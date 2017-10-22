eefbf7f28a78db881085027282966b80d24a2bdd
Дмитрий Мурзин <murzin.da@phystech.edu>
2017-10-22 22:52:42 +0300

    Обновление версии дампа

--- cpp-frequencies/scripts/download-ruwiki-my.sh
+++ cpp-frequencies/scripts/download-ruwiki-my.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 cd `dirname $0`/..
-curl https://dumps.wikimedia.org/ruwiki/20170901/ruwiki-20170901-pages-articles.xml.bz2 | bunzip2 | cmake-build-debug/converter
+curl https://dumps.wikimedia.org/ruwiki/20171020/ruwiki-20171020-pages-articles.xml.bz2 | bunzip2 | cmake-build-debug/converter
