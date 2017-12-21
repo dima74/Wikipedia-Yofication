8d11b8cf444d044fd40422fcd8dfbed1a3d46864
Дмитрий Мурзин <murzin.da@phystech.edu>
2017-12-21 16:43:09 +0300

    backup

---
+++ web-backend/BUILD.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+set -x
+
+# cd && git clone git@github.com:dima74/Wikipedia-Yofication.git
+mkdir -p /home/dima/logs
+
+sudo -s <<EOF
+pacman -S --needed gunicorn python-gevent python-flask python-raven
+ln -s /home/dima/Wikipedia-Yofication/web-backend/gunicorn-wikipedia-yofication.service /etc/systemd/system/
+systemctl enable gunicorn-wikipedia-yofication.service
+systemctl start gunicorn-wikipedia-yofication.service
+
+ln -s /home/dima/Wikipedia-Yofication/nginx/remote /etc/nginx/yofication
+ln -s /etc/nginx/yofication/server-without-ssl.conf /etc/nginx/sites-enabled/yofication.conf
+nginx -s reload
+certbot certonly --nginx --email diraria+ssl@yandex.ru -d yofication.diraria.ru
+
+ln -sf /etc/nginx/yofication/server.conf /etc/nginx/sites-enabled/yofication.conf
+nginx -s reload
