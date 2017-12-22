c447042cd32da1788b5151c6650a4588955da5c8
Дмитрий Мурзин <murzin.da@phystech.edu>
2017-12-22 14:44:00 +0300

    backup

--- cpp-frequencies/scripts/download-ruwiki-my.sh
+++ cpp-frequencies/scripts/download-ruwiki-my.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -e
 cd `dirname $0`/..
-curl https://dumps.wikimedia.org/ruwiki/20171020/ruwiki-20171020-pages-articles.xml.bz2 | bunzip2 | cmake-build-debug/converter
+# https://dumps.wikimedia.org/backup-index.html
+# ruwiki -> ruwiki-*-pages-articles.xml.bz2
+curl https://dumps.wikimedia.org/ruwiki/20171201/ruwiki-20171201-pages-articles.xml.bz2 | bunzip2 | cmake-build-debug/converter
--- web-backend/BUILD.sh
+++ web-backend/BUILD.sh
@@ -4,21 +4,20 @@ set -x
 # cd && git clone git@github.com:dima74/Wikipedia-Yofication.git
 mkdir -p /home/dima/logs
 
-sudo -s <<EOF
-pacman -S --needed gunicorn python-gevent python-flask python-raven cmake
+sudo pacman -S --needed gunicorn python-gevent python-flask python-raven cmake
 
 # files from wikipedia dump
 /home/dima/Wikipedia-Yofication/cpp-frequencies/scripts/all.sh
 
 # nginx
-ln -s /home/dima/Wikipedia-Yofication/web-backend/gunicorn-wikipedia-yofication.service /etc/systemd/system/
-systemctl enable gunicorn-wikipedia-yofication.service
-systemctl start gunicorn-wikipedia-yofication.service
+sudo ln -s /home/dima/Wikipedia-Yofication/web-backend/gunicorn-wikipedia-yofication.service /etc/systemd/system/
+sudo systemctl enable gunicorn-wikipedia-yofication.service
+sudo systemctl start gunicorn-wikipedia-yofication.service
 
-ln -s /home/dima/Wikipedia-Yofication/nginx/remote /etc/nginx/yofication
-ln -s /etc/nginx/yofication/server-without-ssl.conf /etc/nginx/sites-enabled/yofication.conf
-nginx -s reload
-certbot certonly --nginx --email diraria+ssl@yandex.ru -d yofication.diraria.ru
+sudo ln -s /home/dima/Wikipedia-Yofication/nginx/remote /etc/nginx/yofication
+sudo ln -s /etc/nginx/yofication/server-without-ssl.conf /etc/nginx/sites-enabled/yofication.conf
+sudo nginx -s reload
+sudo certbot certonly --nginx --email diraria+ssl@yandex.ru -d yofication.diraria.ru
 
-ln -sf /etc/nginx/yofication/server.conf /etc/nginx/sites-enabled/yofication.conf
-nginx -s reload
+sudo ln -sf /etc/nginx/yofication/server.conf /etc/nginx/sites-enabled/yofication.conf
+sudo nginx -s reload
