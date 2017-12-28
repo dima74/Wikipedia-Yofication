b792a1bf906e3aa23c28b38e5097ac8be6473305
Дмитрий Мурзин <murzin.da@phystech.edu>
2017-12-28 20:51:46 +0300

    backup

--- cpp-frequencies/scripts/all.sh
+++ cpp-frequencies/scripts/all.sh
@@ -6,6 +6,5 @@ rm -rf ../replaces
 ruwiki_creator_script=./print-ruwiki-my-from-bz2-file-to-stdout.sh
 
 ./cmake-init.sh
-eval $ruwiki_creator_script | ./create-all-yowords-from-stdin.sh
-eval $ruwiki_creator_script | ./create-all-pages-from-stdin.sh
 eval $ruwiki_creator_script | ./create-frequencies-from-stdin.sh
+eval $ruwiki_creator_script | ./create-all-pages-from-stdin.sh
