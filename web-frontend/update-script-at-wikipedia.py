e7364a4b0931e0790856f8832a55d0cc8239b46f
Дмитрий Мурзин <diraria@yandex.ru>
2018-05-23 17:53:13 +0300

    Rewrite .travis.yml to don't use archlinux image

--- web-frontend/update-script-at-wikipedia.py
+++ web-frontend/update-script-at-wikipedia.py
@@ -51,5 +51,5 @@ def editPage(title, text):
 
 # run_npm_build()
 login()
-script_text = open(os.argv[1]).read()
+script_text = open('yoficator.bundle.min.js').read()
 editPage(yoficator_page_name, script_text)
