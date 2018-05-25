0b9361dc6c3a52935aa1961571181c315f1012ea
Дмитрий Мурзин <diraria@yandex.ru>
2018-05-25 23:37:50 +0300

    Add automatic updates for all-pages.txt

--- cpp-frequencies/scripts/all.sh
+++ cpp-frequencies/scripts/all.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 cd `dirname $0`
 rm -rf ../replaces
 
@@ -6,5 +6,5 @@ ruwiki_creator_script=./download-ruwiki-my-to-stdout.sh
 #ruwiki_creator_script=./print-ruwiki-my-from-bz2-file-to-stdout.sh
 
 ./cmake-init.sh
-eval $ruwiki_creator_script | ./create-frequencies-from-stdin.sh
-#eval $ruwiki_creator_script | ./create-all-pages-from-stdin.sh
+# https://superuser.com/a/7458/734823
+eval $ruwiki_creator_script | tee >(./create-frequencies-from-stdin.sh) >(./create-all-pages-from-stdin.sh) >/dev/null
