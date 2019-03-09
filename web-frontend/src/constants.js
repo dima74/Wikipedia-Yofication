3adf8ef4ac6b06a6ded92158419e641983d29d62
Dmitry Murzin <diraria@yandex.ru>
2019-03-10 01:24:51 +0300

    Исправлено отсутствие иконки скрипта в панели инструментов

--- web-frontend/src/constants.js
+++ web-frontend/src/constants.js
@@ -3,4 +3,4 @@ export const BACKEND_HOST = process.env.NODE_ENV === 'development'
     : 'https://yofication.herokuapp.com';
 
 export const YO_IMAGE_URL_22 = 'https://yofication.herokuapp.com/static/yo_22.png';
-export const YO_IMAGE_URL_20 = 'https://yofication.herokuapp.com/static/yo.png';
+export const YO_IMAGE_URL_20 = 'https://yofication.herokuapp.com/static/yo_20.png';
